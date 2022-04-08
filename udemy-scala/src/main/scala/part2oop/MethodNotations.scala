8115af71c82b911044fa34484804f4e0d450eb77
Raphael Oliveira <raphael.oliveira@sensedata.com.br>
2022-04-08 16:12:21 -0300

    .

--- udemy-scala/src/main/scala/part2oop/MethodNotations.scala
+++ udemy-scala/src/main/scala/part2oop/MethodNotations.scala
@@ -1,5 +1,7 @@
 package part2oop
 
+import scala.language.postfixOps
+
 
 object MethodNotations extends App {
     class Person(val name: String, favoriteMovie: String) {
@@ -15,20 +17,27 @@ object MethodNotations extends App {
         def +(person: Person): String = {
             s"${this.name} and ${person.name}"
         }
+
+        def unary_! : String = s"$name, what the heck?!"
+
+        def isAlive: Boolean = true
     }
 
     val mary = new Person("Mary", "Inception")
-    println(mary.likes("Inception"))
+    
 
-    println(mary likes "Inception")
-    // infix notation = operator notation
+    // INFIX notation = operator notation
+    println("######### INFIX notation #########")
+    println(s"""mary likes "Inception" -> ${mary likes "Inception"}""")
+    println(mary.likes("Inception"))
+    // are equivalent
 
     // operators in Scala
     val tom = new Person("Tom", "The Arrival")
 
     println(mary hangOutWith tom)
 
-    println(mary + tom)
+    println(s"""mary + tom -> ${mary + tom}""")
     println(mary.+(tom))
 
     println(1 + 2)
@@ -36,6 +45,22 @@ object MethodNotations extends App {
 
     // all operators are methods
 
-    // prefix notation
+    // PREFIX notation
+    // are equivalent
+    println("######### PREFIX notation #########")
+    val x = -1
+    val y = 1.unary_-
+    // unary_ prefix only works with - + ~ !
+    
+    // PREFIX notation 
+    // are equivalent
+    println(s"""!mary -> ${!mary}""")
+    println(s"""mary.unary_! -> ${mary.unary_!}""")
+
+    // POSTFIX notation
+    // doesnt have any arguments
+    println("######### POSTFIX notation #########")
+    println(mary.isAlive)
+    println(s"""mary isAlive -> ${mary isAlive}""")
 
 }
