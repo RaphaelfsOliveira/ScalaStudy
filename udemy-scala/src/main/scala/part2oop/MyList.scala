30fe3f944c537e9fa8e210c82c97cf65723395d9
Raphael Oliveira <raphael.oliveira@sensedata.com.br>
2022-04-11 19:30:18 -0300

    .

--- udemy-scala/src/main/scala/part2oop/MyList.scala
+++ udemy-scala/src/main/scala/part2oop/MyList.scala
@@ -1,31 +1,43 @@
 package part2oop
 
 abstract class MyList {
-    def head: Int
+    def head: Any
     def tail: MyList
     def isEmpty: Boolean
-    def add(element: Int): MyList
-    // def toString: String
+    def add(element: Any): MyList
+    def printElements: String
+
+    // chamada polimorfica
+    override def toString: String = s"[${printElements}]"
 }
 
 object Empty extends MyList {
-    def head: Int = throw new NoSuchElementException
+    def head: Any = throw new NoSuchElementException
     def tail: MyList = throw new NoSuchElementException
     def isEmpty: Boolean = true
-    def add(element: Int): MyList = new Cons(element, Empty)
-    // def toString: String = ???
+    def add(element: Any): MyList = new Cons(element, Empty)
+    def printElements: String = "[]"
 }
 
 
-class Cons(h: Int, t: MyList) extends MyList {
-    def head: Int = h
+class Cons(h: Any, t: MyList) extends MyList {
+    def head: Any = h
     def tail: MyList = t
     def isEmpty: Boolean = false
-    def add(element: Int): MyList = new Cons(element, this)
-    // def toString: String = ???
+    def add(element: Any): MyList = new Cons(element, this)
+    def printElements: String = {
+        if (t.isEmpty) "" + h
+        else h + ", " + t.printElements
+    }
 }
 
 object ListTest extends App {
-    val list = new Cons(1, Empty)
+    val list = new Cons("t", Empty)
     println(list.head)
+    println(list.toString)
+
+    val list2 = list.add(1)
+    val list3 = list2.add("test")
+
+    println(list3.toString)
 }
