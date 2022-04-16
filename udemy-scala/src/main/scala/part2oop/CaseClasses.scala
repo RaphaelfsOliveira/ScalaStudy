406d9b7e8fa801064aea08a590e7040b9a5a5ba4
Raphael Oliveira <raphael.oliveira@saudeid.com.br>
2022-04-16 16:31:46 -0300

    .

---
+++ udemy-scala/src/main/scala/part2oop/CaseClasses.scala
@@ -0,0 +1,33 @@
+package part2oop
+
+object CaseClasses extends App {
+
+  case class Person(name: String, age: Int)
+
+  val jack = new Person(name="Jack", age=35)
+
+  // 1. class parameters are fields there is no need to use val
+  println(jack.name)
+  println(jack.age)
+
+  // 2. sensible toString
+  println(jack.toString)
+  println(jack)
+
+  println(jack.isInstanceOf[Person])
+
+  // 3. equals and hashCode implemented OOTB
+  val john = new Person(name="John", age=37)
+  val jack2 = new Person(name="Jack", age=35)
+
+  println(s"john == jack -> ${john == jack}")
+  println(s"jack == jack2 -> ${jack == jack2}")
+
+  // 4. CCs have handy copy method
+  val jack3 = jack.copy()
+  val jack4 = jack.copy(age=45) // copy receive parameters
+
+  println(s"jack3 == jack2 -> ${jack3 == jack2}")
+  println(jack4)
+
+}
