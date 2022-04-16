180136a3a7fe04541c1caa276208de7c20ac0067
Raphael Oliveira <raphael.oliveira@saudeid.com.br>
2022-04-16 17:27:27 -0300

    .

--- udemy-scala/src/main/scala/part2oop/CaseClasses.scala
+++ udemy-scala/src/main/scala/part2oop/CaseClasses.scala
@@ -1,19 +1,23 @@
 package part2oop
 
+
 object CaseClasses extends App {
 
   case class Person(name: String, age: Int)
 
   val jack = new Person(name="Jack", age=35)
 
+  println("### 1. class parameters are fields there is no need to use val ###")
   // 1. class parameters are fields there is no need to use val
   println(jack.name)
   println(jack.age)
 
+  println("### 2. sensible toString ###")
   // 2. sensible toString
   println(jack.toString)
   println(jack)
 
+  println("### 3. equals and hashCode implemented OOTB ###")
   // 3. equals and hashCode implemented OOTB
   val john = new Person(name="John", age=37)
   val jack2 = new Person(name="Jack", age=35)
@@ -21,6 +25,7 @@ object CaseClasses extends App {
   println(s"john == jack -> ${john == jack}")
   println(s"jack == jack2 -> ${jack == jack2}")
 
+  println("### 4. CCs have handy copy method ###")
   // 4. CCs have handy copy method
   val jack3 = jack.copy()
   val jack4 = jack.copy(age=45) // copy receive parameters
@@ -28,14 +33,34 @@ object CaseClasses extends App {
   println(s"jack3 == jack2 -> ${jack3 == jack2}")
   println(jack4)
 
+  println("### 5. CCs have companion objects ###")
   // 5. CCs have companion objects
   val thePerson = Person
   val mary = Person("Mary", 23)
 
   println(s"companion objects -> ${thePerson.toString}")
   println(mary.isInstanceOf[Person])
-
   println(mary.toString)
-  
+
+  println("### 6. CCs are serializable ###")
+  // 6. CCs are serializable
+  // upickle
+
+  case class City(name: String, funActivity: String, latitude: Double)
+
+  implicit val cityRW = upickle.default.macroRW[City]
+
+  val bengaluru = City("Bengaluru", "South Indian food", 12.97)
+  val city = upickle.default.write(bengaluru)
+  println(city)
+
+  // deserializing case class with JSON
+  val jsonData = """{"name":"Barcelona","funActivity":"Eat tapas","latitude":41.39}"""
+  val barcelona = upickle.default.read[City](jsonData)
+
+  println(barcelona.getClass) // City
+  println(s"name -> ${barcelona.name}")
+  println(s"funActivity -> ${barcelona.funActivity}")
+  println(s"latitude -> ${barcelona.latitude}") // 41.39
 
 }
