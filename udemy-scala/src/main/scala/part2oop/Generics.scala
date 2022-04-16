608a5b529551ded92c150a909d9d5b944e121af9
Raphael Oliveira <raphael.oliveira@saudeid.com.br>
2022-04-15 21:25:27 -0300

    .

--- udemy-scala/src/main/scala/part2oop/Generics.scala
+++ udemy-scala/src/main/scala/part2oop/Generics.scala
@@ -1,5 +1,20 @@
 package part2oop
 
 object Generics extends App {
-    
+
+    class MyList[T] {
+//        usar T para definir o tipo da lista
+    }
+
+    class MyMap[String, Value]
+
+    val listInt = new MyList[Int]
+    val listString = new MyList[String]
+
+    //    generic methods
+    object MyList {
+        def empty[A]: MyList[A] = ???
+
+    }
+
 }
