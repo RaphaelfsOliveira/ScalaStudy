a5ef316a0bb73a852962776c9f1ab840c0349078
Raphael Oliveira <raphael.oliveira@saudeid.com.br>
2022-04-18 06:20:55 -0300

    .

---
+++ udemy-scala/src/main/scala/part3fp/WhatsAFunction.scala
@@ -0,0 +1,44 @@
+package part3fp
+
+object WhatsAFunction extends App {
+  // DREAM: use functions as first class elements
+  // problem: oop
+
+  val doubler = new MyFunction[Int, Int] {
+    override def apply(ele: Int): Int = ele * 2
+  }
+
+  println(doubler(2))
+
+  // function types = Function[A, B]
+  val stringToIntConverter = new Function[String, Int] {
+    override def apply(string: String): Int = string.toInt
+  }
+
+  println(stringToIntConverter("3") + 4)
+
+  def concatenator: (String, String) => String = new Function2[String, String, String] {
+    override def apply(a: String, b: String): String = a + b
+  }
+
+  println(concatenator("test ", "ops"))
+
+  // high-order function
+  val superAdder: Function1[Int, Function1[Int, Int]] = new Function1[Int, Function[Int, Int]] {
+    override def apply(x: Int): Function1[Int, Int] = new Function1[Int, Int] {
+      override def apply(y: Int): Int = x + y
+    }
+  }
+
+  val adder3 = superAdder(3)
+  println(adder3(4))
+
+  println(superAdder(3)(4))
+
+}
+
+trait MyFunction[A, B] {
+  def apply(ele: A): B
+}
+
+
