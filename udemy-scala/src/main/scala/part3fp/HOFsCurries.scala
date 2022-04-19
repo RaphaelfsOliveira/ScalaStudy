e7cee60d2e978b358265a9538ac10be3e0941c5e
Raphael Oliveira <raphael.oliveira@saudeid.com.br>
2022-04-19 11:35:44 -0300

    .

--- udemy-scala/src/main/scala/part3fp/HOFsCurries.scala
+++ udemy-scala/src/main/scala/part3fp/HOFsCurries.scala
@@ -4,10 +4,12 @@ import scala.annotation.tailrec
 
 object HOFsCurries extends App {
 
-//  val superFunction: (Int, (String, (Int => Boolean)) => Int) => (Int => Int) = ???
+  val superFunction: (Int, (String, (Int => Boolean)) => Int) => (Int => Int) = null
   // higher order function (HOF)
 
   // map, flatMap, filter
+
+  println("### higher order function (HOF) ###")
 
   @tailrec
   def nTimes(f: Int => Int, n: Int, x: Int): Int = {
@@ -15,8 +17,39 @@ object HOFsCurries extends App {
     else nTimes(f, n - 1, f(x))
   }
 
-  val plusOne = (x: Int) => x + 1
-  val plusOne2: Int => Int = _ + 1
+  val plusOne = (x: Int) => x + 1 // are equivalent
+  val plusOne2: Int => Int = _ + 1 // are equivalent
+
+  println(nTimes(plusOne, 10, 1))
   println(nTimes(plusOne2, 10, 1))
+  println(nTimes(_ + 1, 10, 1))
+
+
+  def nTimesBetter(f: Int => Int, n: Int): (Int => Int) = {
+    if (n <= 0) (x: Int) => x
+    else (x: Int) => nTimesBetter(f, n - 1)(f(x))
+  }
+
+  val plus10 = nTimesBetter(plusOne2, 11)
+  println(plus10(1))
+
+  println("\n### curried functions ###")
+  // curried functions
+  val superAdder: Int => (Int => Int) = (x: Int) => (y: Int) => x + y
+  val add3 = superAdder(4)
+  println(add3(10))
+  println(superAdder(4)(10))
+
+
+  println("\n### functions with multiple params ###")
+  // functions with multiple params
+  def curriedFormatter(c: String)(x: Double): String = c.format(x)
+
+  val standartFormat: (Double => String) = curriedFormatter("%4.2f")
+  val preciseFormat: (Double => String) = curriedFormatter("%10.8f")
+
+  println(standartFormat(Math.PI))
+  println(preciseFormat(Math.PI))
+
 
 }
