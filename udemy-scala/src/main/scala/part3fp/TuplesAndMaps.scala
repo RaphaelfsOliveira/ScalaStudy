8c8196cc627e5b566358af46b84992023d01ca7b
Raphael Oliveira <raphael.oliveira@saudeid.com.br>
2022-04-23 16:17:20 -0300

    .

--- udemy-scala/src/main/scala/part3fp/TuplesAndMaps.scala
+++ udemy-scala/src/main/scala/part3fp/TuplesAndMaps.scala
@@ -91,8 +91,23 @@ object TuplesAndMaps extends App {
         val friendsA = network(a)
         val friendsB = network(b)
 
-        network + (a -> (friendsA - b) + (b -> friendsB - a))
+        network + (a -> (friendsA - b)) + (b -> (friendsB - a))
     }
 
+    def remove(network: Map[String, Set[String]], person: String): Map[String, Set[String]] = {
+
+        def removeAux(friends: Set[String], networkAcc: Map[String, Set[String]]): Map[String, Set[String]] = {
+            if (friends.isEmpty) networkAcc
+            else removeAux(friends.tail, unfriend(networkAcc, person, friends.head))
+        }
+
+        val unfriended = removeAux(network(person), network)
+        unfriended - person
+    }
+
+    val empty: Map[String, Set[String]] = Map()
+    val network = add(add(empty, "Bob"), "Mary")
+
+    println(network)
 
 }
