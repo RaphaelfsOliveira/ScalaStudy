e8849737b62ac17aa21f069e54857d63eb174986
Raphael Oliveira <raphael.oliveira@saudeid.com.br>
2022-04-24 16:47:18 -0300

    .

--- udemy-scala/src/main/scala/part3fp/Options.scala
+++ udemy-scala/src/main/scala/part3fp/Options.scala
@@ -74,6 +74,12 @@ object Options extends App {
     .foreach(println)
 
   // for-comprehensions
-  
+  val forConnectionStatus = for {
+    host <- config.get("host")
+    port <- config.get("port")
+    connection <- Connection(host, port)
+  } yield connection.connect
+
+  forConnectionStatus.foreach(println)
 
 }
