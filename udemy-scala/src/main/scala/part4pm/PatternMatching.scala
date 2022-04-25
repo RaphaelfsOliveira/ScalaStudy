866d826de254ec991b8fc433b734a5f3d7ca088e
Raphael Oliveira <raphael.oliveira@saudeid.com.br>
2022-04-25 13:54:14 -0300

    .

--- udemy-scala/src/main/scala/part4pm/PatternMatching.scala
+++ udemy-scala/src/main/scala/part4pm/PatternMatching.scala
@@ -31,6 +31,7 @@ object PatternMatching extends App {
   1. cases are matched in order
   2. what if no cases match ? MatchError
   3. type of PM expressions ? unified type of all types in all cases
+  4. PM works really well with case classes
    */
 
   // PM on sealed hierarchies (tem que ter todos os padrões se não pode dar erro)
@@ -44,6 +45,32 @@ object PatternMatching extends App {
     case Dog(name) => println(s"Matched a dog with name $name")
     case Cat(name) => println(s"Matched a cat with name $name")
   }
+
+  // Exercises
+  trait Expr
+  case class Number(n: Int) extends Expr
+  case class Sum(a: Expr, b: Expr) extends Expr
+  case class Prod(a: Expr, b: Expr) extends Expr
+
+  def show(e: Expr): String = e match {
+    case Number(n) => s"$n"
+    case Sum(a, b) => show(a) + " + " show(b)
+    case Prod(a, b) => {
+      def maybeShowParentheses(expr: Expr) = expr math {
+        case Prod(_, _) => show(expr)
+        case Number(_) => show(expr)
+        case _ => "(" + show(expr) + ")"
+      }
+      maybeShowParentheses(a) + " * " + maybeShowParentheses(b)
+    }
+  }
+
+  println(show(Sum(Number(2), Number(3))))
+  println(show(Sum(Sum(Number(2), Number(3))), Number(4)))
+  println(show(Prod(Sum(Number(2), Number(1)), Number(3))))
+  println(show(Sum(Prod(Number(2), Number(1)), Number(3))))
+
+
 
 
 
