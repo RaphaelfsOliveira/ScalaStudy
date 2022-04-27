b7927cc18bd89a5c376e043664f01474bfdad096
Raphael Oliveira <raphael.oliveira@saudeid.com.br>
2022-04-27 15:20:28 -0300

    .

--- udemy-scala/src/main/scala/part4pm/BracelessSyntax.scala
+++ udemy-scala/src/main/scala/part4pm/BracelessSyntax.scala
@@ -63,12 +63,13 @@ object BracelessSyntax {
     case _ => "something else"
 
   // or
-
   val aPatternMatch =
     meaningOfLife match
       case 1 => "the One"
       case 2 => "double or nothing"
       case _ => "something else"
+
+  // methods without braces
 
   def main(args: Array[String]): Unit = {}
 
